188abb55021ffb293c09616e4c66080ca66ee002
ryanch0 <tjddnfkdls0@naver.com>
2026-01-05 21:40:30 +0900

    feat: mobile navigation menu

--- src/components/layout/TobBarForMobile.tsx
+++ src/components/layout/TobBarForMobile.tsx
@@ -1,31 +1,27 @@
 import { useState } from 'react'
 import { cn } from '@/styles/utils.ts'
+import NavMenuList from '../shared/NavForDesktop/NavMenuList'
+import { Menu } from 'lucide-react'
 
 const TobBarForMobile = () => {
   const [toggleMenu, setToggleMenu] = useState(false)
   return (
-    <header className={cn('md:hidden border-secondary border-b p-4')}>
-      <div className={cn('flex')}>
-        <h2 className={cn('typo-page-title')}>Untitled UI</h2>
-        <div
-          className={cn('ml-auto md:hidden text-2xl')}
-          onClick={() => {
-            setToggleMenu(!toggleMenu)
-          }}
-        >
-          ☰
+    <>
+      <header className={cn('md:hidden border-secondary border-b p-4')}>
+        <div className={cn('flex')}>
+          <h2 className={cn('typo-page-title')}>Untitled UI</h2>
+          <button
+            className={cn('ml-auto md:hidden text-secondary cursor-pointer')}
+            onClick={() => {
+              setToggleMenu(!toggleMenu)
+            }}
+          >
+            <Menu size={24} />
+          </button>
         </div>
-      </div>
-      {toggleMenu && (
-        <nav className={cn('md:hidden')}>
-          <ul>
-            <li>mobile-menu</li>
-            <li>mobile-menu</li>
-            <li>mobile-menu</li>
-          </ul>
-        </nav>
-      )}
-    </header>
+      </header>
+      {toggleMenu && <NavMenuList classname={cn('md:hidden py-2 px-1')} />}
+    </>
   )
 }
 export default TobBarForMobile
--- src/components/shared/NavForDesktop/NavForDesktop.tsx
+++ src/components/shared/NavForDesktop/NavForDesktop.tsx
@@ -1,29 +1,14 @@
 import SearchBar from '../SearchBar.tsx'
 import { cn } from '@/styles/utils.ts'
-import ToggleNavItem from '@/components/shared/NavForDesktop/ToggleNavItem.tsx'
-import {
-  ChartBar,
-  ChartPie,
-  ExternalLink,
-  House,
-  LayoutTemplate,
-  MessageCircle,
-  Rows2,
-  Settings,
-  SquareCheckBig,
-  Users,
-  X
-} from 'lucide-react'
-import RedirectNavItem from '@/components/shared/NavForDesktop/RedirectNavItem.tsx'
+import { X } from 'lucide-react'
 import ProgressCircle from '@/components/shared/ProgressCircle.tsx'
 import Profile from '@/components/shared/Profile.tsx'
+import NavMenuList from '@/components/shared/NavForDesktop/NavMenuList.tsx'
 
 type Props = {
   className?: string
 }
 const NavForDesktop = ({ className }: Props) => {
-  const IS_ONLINE_STATUS = true
-
   return (
     <aside
       className={cn(
@@ -36,81 +21,7 @@ const NavForDesktop = ({ className }: Props) => {
           <h2 className={cn('typo-page-title')}>Untitled UI</h2>
           <SearchBar />
         </header>
-        <nav className={cn('flex flex-col h-full typo-nav ')}>
-          <ul>
-            <li>
-              <ToggleNavItem label={'Home'} icon={<House size={20} />} />
-            </li>
-            <li>
-              <ToggleNavItem
-                label={'Dashboard'}
-                icon={<ChartBar size={20} />}
-              />
-            </li>
-            <li>
-              <ToggleNavItem label={'Projects'} icon={<Rows2 size={20} />} />
-            </li>
-            <li>
-              <ToggleNavItem
-                label={'Tasks'}
-                icon={<SquareCheckBig size={20} />}
-                notifications={8}
-              />
-            </li>
-            <li>
-              <ToggleNavItem
-                label={'Reporting'}
-                icon={<ChartPie size={20} />}
-              />
-            </li>
-            <li>
-              <ToggleNavItem label={'Users'} icon={<Users size={20} />} />
-            </li>
-          </ul>
-          <div className={'flex-1'}></div>
-
-          <ul>
-            <li>
-              <RedirectNavItem
-                label={'Settings'}
-                leftIcon={<Settings size={20} />}
-              />
-            </li>
-
-            <li>
-              <RedirectNavItem
-                label={'Support'}
-                leftIcon={<MessageCircle size={20} />}
-                rightIcon={
-                  <label
-                    className={cn(
-                      'bg-primary border-primary border flex gap-1 items-center py-0.5 px-1.5 rounded-md'
-                    )}
-                  >
-                    <div
-                      className={cn(
-                        'size-1.5 rounded-[50%]',
-                        IS_ONLINE_STATUS && 'bg-success '
-                      )}
-                    ></div>
-                    <span className={cn('typo-badge text-muted')}>
-                      {IS_ONLINE_STATUS ? 'Online' : 'Offline'}
-                    </span>
-                  </label>
-                }
-              />
-            </li>
-
-            <li>
-              <RedirectNavItem
-                label={'Open in browser'}
-                leftIcon={<LayoutTemplate size={20} />}
-                rightIcon={<ExternalLink size={16} />}
-              />
-            </li>
-          </ul>
-        </nav>
-
+        <NavMenuList />
         <div className={cn('h-52 bg-secondary p-4 space-y-3')}>
           <div className={cn('flex justify-between')}>
             <ProgressCircle value={80} />
