6a630fb11ef7a672469e1127bc765a38c36b5bbc
ryanch0 <tjddnfkdls0@naver.com>
2026-01-05 22:20:53 +0900

    chore: add dark-mode trigger

--- src/components/pages/SettingsPage.tsx
+++ src/components/pages/SettingsPage.tsx
@@ -2,8 +2,22 @@ import Layout from '../layout/Layout.tsx'
 import { cn } from '@/styles/utils.ts'
 import SettingsHeader from '@/components/features/settings/components/SettingsHeader.tsx'
 import SettingsForm from '@/components/features/settings/components/SettingsForm/SettingsForm.tsx'
+import { useEffect } from 'react'
 
 const SettingsPage = () => {
+  useEffect(() => {
+    const updateTheme = () => {
+      const params = new URLSearchParams(window.location.search)
+      const dark = params.get('dark') === 'true'
+      document.documentElement.classList.toggle('dark', dark)
+    }
+
+    updateTheme()
+    window.addEventListener('popstate', updateTheme)
+
+    return () => window.removeEventListener('popstate', updateTheme)
+  }, [])
+
   return (
     <Layout>
       <main className={cn('bg-primary px-4 pb-12 pt-8 md:px-8')}>
